2046892c9e023a70ae975538541247e266f8c4c8
Ayodele abiola Augustine <ayodelaustinab@gmail.com>
2020-08-27 09:13:42 +0100

    create groupby data for all arrributes

--- data analysis.py
+++ data analysis.py
@@ -7,7 +7,22 @@ ex_employees = pd.read_excel(
     sheet_name="Employees who have left")
 
 data = ex_employees.groupby("dept")
+
+
 satisfaction_mean = data.mean()["satisfaction_level"]
+
+average_monthly_hour_mean = data.mean()["average_montly_hours"]
+
+number_project_mean = data.mean()["number_project"]
+
+time_spend_company_mean = data.mean()["time_spend_company"]
+
+last_evaluation_mean = data.mean()["last_evaluation"]
+
+promotion_last_5years_mean = data.mean()["promotion_last_5years"]
+
+work_accident_mean = data.mean()["Work_accident"]
+
 
 
 
